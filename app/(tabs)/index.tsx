cbb6d23ee49a2f76b5b8ce709e3cd223b1fc8e17
Juliana Rodrigues <127902970+juulianarb@users.noreply.github.com>
2024-12-10 20:40:25 -0300

    Add files via upload

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+export default function TextoSimples() {
+  return (
+   <View> 
+   <Text>Olá, meu nome é Juliana Rodrigues, e este é um exemplo de texto simples!</Text>
+   </View>
+  );
+}
